4525fbfd94fb3d43f4f80c55ffb4b8db098a307b
enayee <32726541+enayee@users.noreply.github.com>
2019-07-29 02:19:09 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let letTopDeck = [];
 const rightTopDeck = [[], [], [], []];
 const underDecks =  [
@@ -13,16 +12,17 @@ const underDecks =  [
 ];
 
 function suffle(array) {
-  console.log('suffle')
-  for (let i = array.length - 1;i > 0; i++) {
+  for (let i = array.length - 1;i > 0; i--) {
     let j = Math.floor(Math.random() * i + 1);
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
 };
 
+const pokeArray = Array.from({length: 52},(_,k) => k+1);
+const sufferedDeck = suffle(pokeArray);
+
 function newCards() {
-  console.log('newCard')
   for (let i=0;i<=7;i++){
     if(i<=3){
         underDecks[i]=sufferedDeck.splice(0,6);
@@ -34,31 +34,52 @@ function newCards() {
 
 newCards();
 
-const pokeArray = Array.from(new Array(52).map((_, index) => {
-  return index + 1
-}));
-const sufferedDeck = suffle(pokeArray);
+function transformNumberToEnglish(cardNumber) {
+  switch (cardNumber) {
+    case 0:
+      return 'K';
+    case 1:
+      return 'A';
+    case 11:
+      return 'J';
+    case 12:
+      return 'Q';
+    default:
+      return cardNumber;
+  }
+}
+
+function transformNumberToPattern(cardNumber) {
+  if (cardNumber > 0 && cardNumber <= 13) return 'mushroom';
+  if (cardNumber >= 14 && cardNumber <= 26) return 'heart';
+  if (cardNumber >= 27 && cardNumber <= 39) return 'diamond';
+  if (cardNumber >= 40 && cardNumber <= 52) return 'flower';
+}
 
 const underDeckComponent = document.getElementById('under-deck');
 underDeckComponent.style.display= "flex";
 underDeckComponent.style.justifyContent = "space-around";
+
 function distributeDesks() {
+  let body = document.body;
+  body.style.backgroundColor = "#222222";
   underDecks.forEach((deck, index) => {
     const underDeck = document.createElement('div');
     underDeck.id = `under-deck-${index + 1}`;
     underDeck.style.position = "relative";
     underDeck.style.height = "600px";
     underDeck.style.width = "130px";
+
     deck.forEach((card, cardIndex) => {
       const underDeckCard = document.createElement('div');
       underDeckCard.style.position = "absolute";
       underDeckCard.style.width = "100%";
-      underDeckCard.style.height = "213px";
+      underDeckCard.style.height = "200px";
       underDeckCard.style.border = "2px solid #FFFFFF";
       underDeckCard.style.color = "#FFFFFF";
       underDeckCard.style.paddingLeft = "8px";
       underDeckCard.style.paddingTop = "4px";
-      underDeckCard.style.top = cardIndex * 20 + "px";
+      underDeckCard.style.top = cardIndex * 40 + "px";
       underDeckCard.style.left = "0px";
       underDeckCard.style.background = "#3D3D3D";
       underDeckCard.style.borderRadius = "4px";
@@ -73,35 +94,21 @@ function distributeDesks() {
             card
           )}.svg' />
         </div>
-        <div style='display: flex; align-items: center, position: absolute; bottom: 6px; right: 8px; transform: rotate(180);'>
+        <div style='display: flex; align-items: center; position: absolute; bottom: 6px; right: 8px; transform: rotate(180deg);'>
+        <img style='margin-left: 2px' src='./img/${transformNumberToPattern(
+          card
+        )}.svg' />
           <span>${transformNumberToEnglish(
             card % 13
-          )}</span><img style='margin-left: 2px' src='./img/${transformNumberToPattern(
-            card
-          )}.svg' />
+          )}</span>
         </div>
         </div>
       `;
+  
+      underDeck.appendChild(underDeckCard);
     })
+    underDeckComponent.appendChild(underDeck);
   })
 }
-function transformNumberToEnglish(cardNumber) {
-  switch (cardNumber) {
-    case 0:
-      return 'K';
-    case 1:
-      return 'A';
-    case 11:
-      return 'J';
-    case 12:
-      return 'Q';
-  }
-}
-function transformNumberToPattern(cardNumber) {
-  if (cardNumber > 0 && cardNumber <= 13) return 'mushroom';
-  if (cardNumber >= 14 && cardNumber <= 26) return 'heart';
-  if (cardNumber >= 27 && cardNumber <= 39) return 'diamond';
-  if (cardNumber >= 40 && cardNumber <= 52) return 'flower';
-}
+
 distributeDesks();
-console.log('hello')
